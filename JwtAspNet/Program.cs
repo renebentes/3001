70dcd81bcb739d548905c9c0a922b839d770e1a6
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-11-08 11:29:35 -0300

    feat: Implementa geração de Token JWT básico

--- JwtAspNet/Program.cs
+++ JwtAspNet/Program.cs
@@ -1,6 +1,10 @@
+using JwtAspNet;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<TokenService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (TokenService tokenService) => tokenService.Create());
 
 app.Run();
---
+++ JwtAspNet/Services/TokenService.cs
@@ -0,0 +1,26 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAspNet;
+
+public class TokenService
+{
+    public string Create()
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(Configuration.PrivateKey);
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256
+        );
+
+        var tokenDescriptor = new SecurityTokenDescriptor{
+            SigningCredentials = signingCredentials,
+            Expires = DateTime.UtcNow.AddHours(2)
+        };
+
+        var token = handler.CreateToken(tokenDescriptor);
+        return handler.WriteToken(token);
+    }
+}
