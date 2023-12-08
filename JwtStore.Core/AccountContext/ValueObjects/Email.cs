2c9ce228e715028a0fa8e081256557486bd2a83b
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-12-08 11:49:34 -0300

    feat: Define objeto de valor para e-mail

--- JwtStore.Core/AccountContext/ValueObjects/Email.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -1,9 +1,33 @@
-﻿namespace JwtStore.Core.AccountContext.ValueObjects;
+﻿using JwtStore.Core.SharedContext.Common;
+
+namespace JwtStore.Core.AccountContext.ValueObjects;
 
 public class Email : ValueObject
 {
+    public Email(string address)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(address);
+
+        Address = address.Trim().ToLower();
+
+        if (Address.Length < 5)
+            throw new Exception("E-mail inválido");
+
+        if (!RegexPatterns.EmailRegex().IsMatch(Address))
+            throw new Exception("E-mail inválido");
+    }
+
     public string Address { get; } = string.Empty;
 
     public string Hash
         => Address.ToBase64();
+
+    public override string ToString()
+        => Address;
+
+    public static implicit operator string(Email email)
+        => email.ToString();
+
+    public static implicit operator Email(string address)
+        => new(address);
 }
---
+++ JwtStore.Core/SharedContext/Common/RegexPatterns.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace JwtStore.Core.SharedContext.Common;
+
+public sealed partial class RegexPatterns
+{
+    private const string EmailPattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+    [GeneratedRegex(EmailPattern)]
+    public static partial Regex EmailRegex();
+}
