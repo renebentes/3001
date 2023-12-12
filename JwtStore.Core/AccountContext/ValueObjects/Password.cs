a7a242d46a0239698c6250625b9bbbbb309f5c91
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-12-12 10:53:28 -0300

    feat: Gera senhas aleatórias

--- JwtStore.Core/AccountContext/ValueObjects/Password.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Password.cs
@@ -2,9 +2,26 @@ namespace JwtStore.Core.AccountContext.ValueObjects;
 
 public sealed class Password : ValueObject
 {
-    private const string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     private const string Special = "!@#$%ˆ&*(){}[];";
+    private const string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
     public string Hash { get; } = string.Empty;
+
     public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper();
+
+    private static string Generate(short length = 16,
+                                   bool includeSpecialChars = true,
+                                   bool upperCase = false)
+    {
+        var chars = includeSpecialChars ? (Valid + Special) : Valid;
+        var startRandom = upperCase ? 26 : 0;
+        var index = 0;
+        var res = new char[length];
+        var rnd = new Random();
+
+        while (index < length)
+            res[index++] = chars[rnd.Next(startRandom, chars.Length)];
+
+        return new string(res);
+    }
 }
