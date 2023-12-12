4ab9468e414fce8a60baabd5e62a83c074d8e848
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-12-12 10:45:35 -0300

    feat: Define objeto de valor base para senhas

---
+++ JwtStore.Core/AccountContext/ValueObjects/Password.cs
@@ -0,0 +1,10 @@
+namespace JwtStore.Core.AccountContext.ValueObjects;
+
+public sealed class Password : ValueObject
+{
+    private const string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    private const string Special = "!@#$%ˆ&*(){}[];";
+
+    public string Hash { get; } = string.Empty;
+    public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper();
+}
