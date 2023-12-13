1e7031d01d6cac2a3c4ae9ac9a4ad8be48a1802d
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-12-13 11:06:56 -0300

    refactor: Utiliza método de extensão para conversão em base64

--- JwtStore.Core/SharedContext/Extensions/StringExtensions.cs
+++ JwtStore.Core/SharedContext/Extensions/StringExtensions.cs
@@ -5,5 +5,5 @@ namespace JwtStore.Core.SharedContext.Extensions;
 public static class StringExtensions
 {
     public static string ToBase64(this string text)
-        => Convert.ToBase64String(Encoding.ASCII.GetBytes(text));
+        => Encoding.ASCII.GetBytes(text).ToBase64();
 }
