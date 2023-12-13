632405424fe4c264907ab9b2f4ddaf938421d6b0
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-12-13 11:15:14 -0300

    feat: Define método para comparar hash de senhas

--- JwtStore.Core/AccountContext/ValueObjects/Password.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Password.cs
@@ -47,4 +47,33 @@ public sealed class Password : ValueObject
 
         return $"{iterations}{splitChar}{salt}{splitChar}{key}";
     }
+
+    private static bool Verify(string hash,
+                               string password,
+                               short keySize = 32,
+                               int iterations = 10000,
+                               char splitChar = '.')
+    {
+        password += Settings.SecretsOptions.PasswordSaltKey;
+
+        var parts = hash.Split(splitChar, 3);
+
+        if (parts.Length != 3)
+            return false;
+
+        var hashIterations = Convert.ToInt32(parts[0]);
+        var salt = Convert.FromBase64String(parts[1]);
+        var key = Convert.FromBase64String(parts[2]);
+
+        if (hashIterations != iterations)
+            return false;
+
+        using var algorithm = new Rfc2898DeriveBytes(password,
+                                                     salt,
+                                                     iterations,
+                                                     HashAlgorithmName.SHA256);
+        var keyToCheck = algorithm.GetBytes(keySize);
+
+        return keyToCheck.SequenceEqual(key);
+    }
 }
