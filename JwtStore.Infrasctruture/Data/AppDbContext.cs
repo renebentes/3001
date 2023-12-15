354b3a47be244c9bf9f29ba1e312947f04a851ae
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-12-15 09:59:39 -0300

    feat(data): Define contexto de dados da aplicação

---
+++ JwtStore.Infrasctruture/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using JwtStore.Core.AccountContext.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtStore.Infrasctruture;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
