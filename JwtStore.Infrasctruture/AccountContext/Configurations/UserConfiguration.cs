029086469adc13e3c8be93ae9499b2cf33bac16c
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-12-15 09:25:55 -0300

    feat: Realiza o mapeamento objeto-relacional da entidade User

---
+++ JwtStore.Infrasctruture/AccountContext/Configurations/UserConfiguration.cs
@@ -0,0 +1,58 @@
+using JwtStore.Core.AccountContext.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JwtStore.Infrastructure.AccountContext.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable(nameof(User));
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Name)
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(120)
+            .IsRequired();
+
+        builder.OwnsOne(u => u.Email)
+            .Property(e => e.Address)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(255)
+            .IsRequired();
+
+        builder.OwnsOne(u => u.Email)
+            .OwnsOne(e => e.Verification)
+            .Property(v => v.Code)
+            .HasColumnType("CHAR")
+            .HasMaxLength(6)
+            .IsRequired();
+
+        builder.OwnsOne(u => u.Email)
+            .OwnsOne(e => e.Verification)
+            .Property(v => v.ExpiresAt);
+
+        builder.OwnsOne(u => u.Email)
+            .OwnsOne(e => e.Verification)
+            .Property(v => v.VerifiedAt);
+
+        builder.OwnsOne(u => u.Email)
+            .OwnsOne(e => e.Verification)
+            .Ignore(v => v.IsActive);
+
+        builder.OwnsOne(u => u.Password)
+            .Property(p => p.Hash)
+            .IsRequired();
+
+        builder.OwnsOne(u => u.Password)
+            .Property(p => p.ResetCode)
+            .IsRequired();
+
+        builder.Property(u => u.Image)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(255)
+            .IsRequired();
+    }
+}
