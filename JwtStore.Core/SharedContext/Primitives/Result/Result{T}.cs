0cbdbe042d923cd09d37db015b346075b3d134d6
Rene B. Pinto <renebentes@yahoo.com.br>
2024-03-28 15:43:18 -0300

    refactor: Move factory methods para Result<TValue>

--- JwtStore.Core/SharedContext/Primitives/Result/Result{T}.cs
+++ JwtStore.Core/SharedContext/Primitives/Result/Result{T}.cs
@@ -1,4 +1,4 @@
-namespace JwtStore.Core.SharedContext.Primitives.Result;
+﻿namespace JwtStore.Core.SharedContext.Primitives.Result;
 
 /// <summary>
 /// Represents a result of operations, with status information and possibly value and errors.
@@ -12,7 +12,7 @@ public class Result<TValue> : Result
     /// Initializes a new instance of the <see cref="Result{TValue}"/> class with specified value.
     /// </summary>
     /// <param name="value">The result value</param>
-    protected internal Result(TValue value)
+    protected Result(TValue value)
         => _value = value;
 
     /// <summary>
@@ -21,7 +21,7 @@ public class Result<TValue> : Result
     /// <param name="value">The result value</param>
     /// <param name="status">The <see cref="ResultStatus"/></param>
     /// <param name="errors">The <see cref="Error"/> collection</param>
-    protected internal Result(TValue value, ResultStatus status, IEnumerable<Error> errors)
+    protected Result(TValue value, ResultStatus status, IEnumerable<Error> errors)
         : base(status, errors)
         => _value = value;
 
@@ -37,4 +37,28 @@ public class Result<TValue> : Result
 
     public static implicit operator Result<TValue>(TValue value)
         => Success(value);
+
+    /// <summary>
+    /// Represents a successful <see cref="Result{TValue}"/> operation with the specified value.
+    /// </summary>
+    /// <typeparam name="TValue">The result type.</typeparam>
+    /// <param name="value">The result value.</param>
+    /// <returns>A new instance of <see cref="Result{TValue}"/> with the success flag set.</returns>
+    public static Result<TValue> Success(TValue value)
+        => new(value);
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="error"></param>
+    /// <returns></returns>
+    public static new Result<TValue> Failure(Error error)
+        => new(default!, ResultStatus.Error, [error]);
+
+    public static new Result<TValue> Failure(params Error[] errors)
+        => new(default!, ResultStatus.Error, new List<Error>(errors));
+
+    public static new Result<TValue> Failure(IEnumerable<Error> errors)
+        => new(default!, ResultStatus.Error, errors);
 }
