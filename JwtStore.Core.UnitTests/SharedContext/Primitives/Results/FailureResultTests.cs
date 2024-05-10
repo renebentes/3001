c57223e83b308d22a26ffd9462c61bcaa3d8752c
Rene Bentes Pinto <renebentes@yahoo.com.br>
2024-05-09 22:57:46 -0300

    test: avalia criação de Result tipado com falha

--- JwtStore.Core.UnitTests/SharedContext/Primitives/Results/FailureResultTests.cs
+++ JwtStore.Core.UnitTests/SharedContext/Primitives/Results/FailureResultTests.cs
@@ -55,4 +55,15 @@ public class FailureResultTests
             result.Errors.Should().ContainEquivalentOf(error);
         }
     }
+
+    [Fact]
+    public void CreateStronglyTypedIntFailureResult()
+    {
+        var error = new Error("error", "error message");
+        var result = Result.Failure<int>(error);
+
+        result.IsSuccess.Should().BeFalse();
+        result.Status.Should().Be(ResultStatus.Error);
+        result.Errors.Should().ContainEquivalentOf(error);
+    }
 }
