66118d448ee1e6ee023d74615b089166d7393375
Rene Bentes Pinto <renebentes@yahoo.com.br>
2024-06-22 10:28:42 -0300

    refactor: define constantes para tamanhos de campos

--- JwtStore.UseCases/Users/CreateUser/CreateUserCommandValidator.cs
+++ JwtStore.UseCases/Users/CreateUser/CreateUserCommandValidator.cs
@@ -2,12 +2,17 @@ namespace JwtStore.UseCases.Users.CreateUser;
 
 public static class CreateUserCommandValidator
 {
+    public const int NameMaxLength = 160;
+    public const int NameMinLength = 3;
+    public const int PasswordMaxLength = 40;
+    public const int PasswordMinLength = 8;
+
     public static Contract<Notification> Ensure(CreateUserCommand command)
         => new Contract<Notification>()
         .Requires()
-        .IsLowerThan(command.Name.Length, 160, nameof(command.Name))
-        .IsGreaterThan(command.Name.Length, 3, nameof(command.Name))
-        .IsLowerThan(command.Password.Length, 40, nameof(command.Password))
-        .IsGreaterThan(command.Password.Length, 8, nameof(command.Password))
+        .IsLowerThan(command.Name.Length, NameMaxLength, nameof(command.Name))
+        .IsGreaterThan(command.Name.Length, NameMinLength, nameof(command.Name))
+        .IsLowerThan(command.Password.Length, PasswordMaxLength, nameof(command.Password))
+        .IsGreaterThan(command.Password.Length, PasswordMinLength, nameof(command.Password))
         .IsEmail(command.Email, nameof(command.Email));
 }
